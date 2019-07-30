2131f440ded83540d22d93f187c5935cab6ffa51
zhuyabei <837765980@qq.com>
2019-07-30 10:48:32 +0800

    新建省市区文件

---
+++ static/config.js
@@ -0,0 +1,17 @@
+const addressList = {
+    List:[
+        {
+            countryName:"United States",
+            countryList:[
+                "亚拉巴马州","阿拉斯加州","亚利桑那州","阿肯色州","加利福尼亚州"
+            ]
+        },
+        {
+            countryName:"Canada",
+            countryList:[
+                "阿尔伯塔省","不列颠哥伦比亚省","马尼托巴省","纽芬兰与拉布拉多省","新不伦瑞克省"
+            ]
+        }
+    ]
+}
+export default {addressList}
