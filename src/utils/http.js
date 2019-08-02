880dc79b72db272eb8ad327ef6775cbb87d3ecf8
zhuyabei <837765980@qq.com>
2019-08-02 08:54:36 +0800

    api

--- src/utils/http.js
+++ src/utils/http.js
@@ -14,10 +14,9 @@ axios.default.headers ={
     'Access-Control-Allow-Credentials':'true',
     'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,PUT,DELETE',
     'Access-Control-Allow-Headers': 'cache-control,content-type,hash-referer,x-requested-with',
-    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Type': 'application/x-www-form-urlencoded,multipart/form-data',
     'Access-Control-Allow-Origin':'*'
 }
-// axios.defaults.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
 
 // request全局拦截
 axios.interceptors.request.use(config => {
@@ -34,6 +33,9 @@ axios.interceptors.request.use(config => {
 
 // response全局拦截
 axios.interceptors.response.use(response => {
+    let token = window.localStorage.getItem("userToken")
+    response.headers.Authorization = `bearer ${token}`;
+    response.headers['Access-Control-Allow-Origin'] = '*';
     return response.data
 }, error => {
     return Promise.reject(error.response)
