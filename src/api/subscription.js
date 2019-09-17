9d373d4980e6d52eaa882cbf883dee6561f8aed3
zhuyabei <837765980@qq.com>
2019-09-17 11:07:55 +0800

    订阅（未登录用户）

---
+++ src/api/subscription.js
@@ -0,0 +1,16 @@
+import { get, post } from "../utils/http.js";
+
+// 未登录用户订阅
+export const Nosubscribe = params=>post("/api/subscribe_no_register",params);
+
+//客户注册是勾选订阅
+export const registeSubscribe = params =>post("/api/register_subscribe_newsletter",params)
+
+//个人中心我的订阅界面展示
+export const mysubscribe = params => post("/api/address/mysubscribe",params)
+
+//个人中心登录客户订阅
+export const yesSubscribe = params =>post("/api/subscribe",params)
+
+//退订界面显示
+export const delSubscribe = params =>post("/api/unsubscribe_index",params)
