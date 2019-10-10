704f922533b0f657638ca9e0839ff1da0f5f989f
huangmeng <meng.huang@panduo.com.cn>
2019-10-10 16:39:31 +0800

    1010-1440

--- src/view/myAccount/page/myCoupon.vue
+++ src/view/myAccount/page/myCoupon.vue
@@ -47,7 +47,7 @@
                                             <div class="Expired" v-if="coupon.cc_coupon_status === 20">Used</div>
                                           </div>
                                           <div class="couponTime">
-                                            <div style="line-height: 40px;width: 120px;color: #a7a7a7">Expired Date:</div>
+                                            <div style="line-height: 34px;width: 120px;color: #a7a7a7">Expired Date:</div>
                                             <div class="timeRange" style="color: #a7a7a7" :class="coupon.cc_coupon_start_time?'':'noStartTime'">
                                               <div v-if="coupon.cc_coupon_start_time">{{coupon.cc_coupon_start_time}}</div>
                                               <div v-if="coupon.cc_coupon_end_time">{{coupon.cc_coupon_end_time}}</div>
