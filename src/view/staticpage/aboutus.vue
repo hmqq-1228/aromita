1871f43700d04d4ee1f7c4965bed617de7224eb9
zhuyabei <837765980@qq.com>
2019-10-10 16:39:35 +0800

    新增静态页，关于我们

---
+++ src/view/staticpage/aboutus.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="aboutus">
+        <h3>ABOUT ANAVIA</h3> 
+        <p>     ANAVIA is a unique and modern jewelry and gift company that is born from pure imagination and graceful affection. Having multiple designs and creative ideas ANAVIA has focused not only on cremation jewelry, fashion and essential oil jewelry, but has also produces gifts for every special moment. Introducing new designs and innovative ideas has made ANAVIA, our brand of pure happiness and a go to place for your ideal designs. We are happy to welcome you to our growing family of everlasting memorial gifts and designs. With open hands we will be with you every step of the way.</p> 
+        <h3>OUR MISSION</h3>    
+        <p>     Led by a professional team with over nine years of experience in the jewelry and design industry, Anavia caters to the world wide stainless steel crafting market. We have assisted in creating memories, art and history with all our ideas and modern concepts in stainless steel products. We stand for jewelry makers, craft professionals and anyone who has created with their hearts. ANAVIA helps and supports anyone and any business that has ever picked up a piece of metal and created something imaginative. The work you do is important to us. Pieces from ANAVIA can be created by you to become heirlooms that transcend time. We highly appreciate the opportunities you give to us as being a part of the ANAVIA Family.</p> 
+        <h3>STORY BEHIND</h3>
+        <p>     ANAVIA As an eternal guardian, the bird of Anavia has held its beauty and grace through the coldest winters and the course of time. As an ancient protector of power, Anavia has an everlasting bound to keep our loved ones safe through the journey of life, death and rebirth. Through Anavia’s past lifetimes she has been reborn from ice to never vanish and always keep heading forward in an eternal path to happiness. Just like we do with our jewelry Anavia is a reflection of soul, love and compassion to those who no longer walk among us but walk in our memories.</p>
+    </div>
+</template>
+<style lang="scss" scoped>
+.aboutus{
+    width: 1200px;
+    margin:0 auto;
+    padding:40px 0;
+    h3{
+        height: 80px;
+        line-height: 80px;
+        color: #333;
+    }
+    p{
+        color: #666;
+        line-height: 30px;
+        text-indent:2em;
+    }
+}
+</style>
